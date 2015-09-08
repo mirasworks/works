8d19a93d1cc4f908323137fe92b1d99e1fabe3ab
DamienMIRAS <damien.miras@gmail.com>
2015-09-08 21:24:08 +0200

    minor comments

--- src/com/mirasworks/server/http/HttpPipelineFactory.java
+++ src/com/mirasworks/server/http/HttpPipelineFactory.java
@@ -32,21 +32,22 @@ public class HttpPipelineFactory implements ChannelPipelineFactory {
         httpServerHandeler.setContext(context);
 
         // Uncomment the following line if you want HTTPS
-        // SSLEngine engine = SecureChatSslContextFactory.getServerContext().createSSLEngine();
-        // engine.setUseClientMode(false);
-        // pipeline.addLast("ssl", new SslHandler(engine));
+		// SSLEngine engine =
+		// SecureChatSslContextFactory.getServerContext().createSSLEngine();
+		// engine.setUseClientMode(false);
+		// pipeline.addLast("ssl", new SslHandler(engine));
 
         //TODO See play to separate the chunck writer
         pipeline.addLast("flashPolicy", new FlashPolicyHandler());
         //simple netty ineritance to debug
         pipeline.addLast("decoder", new RequestDecoder());
         // Uncomment the following line if you don't want to handle HttpChunks.
-        // pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
+        //pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
+        //TODO rewrite the httpResponse encoder to handle a worksResponse and avoid the copy
         pipeline.addLast("encoder", new HttpResponseEncoder());
         // Remove the following line if you don't want automatic content compression.
         //pipeline.addLast("deflater", new HttpContentCompressor());
         //uncomment to see the request back
-        //pipeline.addLast("handler", new HttpEchoServerHandler());
         pipeline.addLast("handler", httpServerHandeler);
         return pipeline;
     }
