e26a69aa603478738cd387e8eeb246590d02a022
DamienMiras <damien.miras@gmail.com>
2015-09-14 02:23:15 +0200

    minor format

--- src/com/mirasworks/server/http/RequestDecoder.java
+++ src/com/mirasworks/server/http/RequestDecoder.java
@@ -8,19 +8,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestDecoder extends HttpRequestDecoder {
-    @SuppressWarnings("unused")
-    private final Logger l = LoggerFactory.getLogger(RequestDecoder.class);
+	@SuppressWarnings("unused")
+	private final Logger l = LoggerFactory.getLogger(RequestDecoder.class);
 
-    @Override
-    protected HttpMessage createMessage(String[] initialLine) throws Exception {
+	@Override
+	protected HttpMessage createMessage(String[] initialLine) throws Exception {
 
-        WorksRequest httpRequest = new WorksRequest(HttpVersion.valueOf(initialLine[2]), HttpMethod.valueOf(initialLine[0]), initialLine[1]);
-        return httpRequest;
-    }
+		WorksRequest httpRequest = new WorksRequest(HttpVersion.valueOf(initialLine[2]),
+				HttpMethod.valueOf(initialLine[0]), initialLine[1]);
+		return httpRequest;
 
-    @Override
-    protected boolean isDecodingRequest() {
-        return true;
-    }
+	}
+
+	@Override
+	protected boolean isDecodingRequest() {
+		return true;
+	}
 
 }
